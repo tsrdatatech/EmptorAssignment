03880b61eec8674db2cf439109123d591506db78
Tom Russell <tsrdatatech@gmail.com>
2020-09-16 10:57:04 -0400

    Adding storing response content to s3, insert title into dynamodb, return s3 url

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,17 +2,36 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from validator_collection import checkers
+import boto3
+import os
+s3 = boto3.client('s3')
 
 
 def title(event, context):
-
     url = event['url']
+
+    # s3 bucket and dynamo table info
+    bucket = os.environ.get('S3BUCKET')
+    table = os.environ.get('DYNAMO_TABLE')
 
     # validate url
     if checkers.is_url(url):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
         page_title = soup.title.string
+
+        key = str(hash(url))
+
+        # s3 operations
+        # s3 = boto3.client('s3')
+        # s3bucket = s3.Bucket(bucket)
+        s3.put_object(Body=response.content, Bucket=bucket, Key=key)
+        s3_url = 'https://%s.s3.amazonaws.com/%s' % (bucket, key)
+
+        # dynamo operations
+        dynamo = boto3.resource('dynamodb')
+        table = dynamo.Table(table)
+        table.put_item(Item={'id': key, 'title': page_title})
 
         if not page_title:
             return {
@@ -26,6 +45,7 @@ def title(event, context):
         return {
             'statusCode': 200,
             'body': json.dumps({"page_title": page_title}),
+            's3_url': s3_url,
             'headers': {
                 'Content-Type': 'application/json',
             }
